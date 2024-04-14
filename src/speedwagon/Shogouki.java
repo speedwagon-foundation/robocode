639bc8e78186f0699f484c7f94bcbc7d4ac7533c
andi4971 <andi4971@gmail.com>
2024-04-14 12:41:32 +0200

    updattee

--- src/speedwagon/Shogouki.java
+++ src/speedwagon/Shogouki.java
@@ -1,9 +1,8 @@
 package speedwagon;
 
 import org.w3c.dom.css.Rect;
-import robocode.AdvancedRobot;
-import robocode.HitByBulletEvent;
-import robocode.ScannedRobotEvent;
+import robocode.*;
+import robocode.util.Utils;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
@@ -13,6 +12,7 @@ import java.util.HashMap;
 public class Shogouki extends AdvancedRobot {
     private static final double MIN_BULLET_POWER = 1.0;
     private static final int WALL_MARGIN = 60;
+    private static final double SCAN_DIST = 36;
     private static final double BEST_DISTANCE = 525; // magic number - trial and error
     private static double blindCane = 130; // distance to wall to start wall smoothing
     private static double battlefieldWidth;
@@ -22,20 +22,29 @@ public class Shogouki extends AdvancedRobot {
     private static double numBadHits = 0;
     private static boolean flat = true;
     private static EnemyInfo currentTarget;
+    Rectangle2D.Double battlefield;
 
     private final HashMap<String, EnemyInfo> detectedEnemies = new HashMap<>();
 
     @Override
     public void run() {
+        initColors();
+
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);
 
         battlefieldWidth = getBattleFieldWidth();
         battlefieldHeight = getBattleFieldHeight();
 
+        // get battlefield - bounds (18) - for some reason upper bounds use 18 * 2 buffer
+        battlefield = new Rectangle2D.Double(18, 18, battlefieldWidth - 36, battlefieldHeight - 36);
+
+
         ahead(Double.MAX_VALUE);
         while (true) {
-            turnRadarRightRadians(Double.POSITIVE_INFINITY);
+            if (getRadarTurnRemaining() == 0.0)
+                setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+            execute();
         }
     }
 
@@ -43,13 +52,17 @@ public class Shogouki extends AdvancedRobot {
     public void onScannedRobot(ScannedRobotEvent e) {
         updateDetectedEnemies(e);
         currentTarget = detectedEnemies.get(e.getName());
+        performMovement(e);
+        updateRadar(e);
+        fireAtEnemy(e);
 
+    }
+
+    private void performMovement(ScannedRobotEvent e) {
         double theta;
         double enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
         double enemyDistance = e.getDistance();
 
-        // get battlefield - bounds (18) - for some reason upper bounds use 18 * 2 buffer
-        Rectangle2D.Double battlefield = new Rectangle2D.Double(18, 18, battlefieldWidth - 36, battlefieldHeight - 36);
 
         // movement
 
@@ -69,29 +82,73 @@ public class Shogouki extends AdvancedRobot {
         theta = absoluteBearing(getX(), getY(), newDestination) - getHeadingRadians();
         setAhead(Math.cos(theta) * 100);
         setTurnRightRadians(Math.tan(theta));
-        /**
-         * ideas:
-         *
-         * don't fire when energy under 30%
-         * don't move into corners, i.e. draw a circle from the center and use it as the border for movement
-         * move away from opponent if spotted and under 600 ranger or something like that
-         *
-         * while no enemy spotted: turn radar in the same direction as the gun for maximum turn rate
-         * once enemy is spotted: try to track enemy with radar and gun, i.e. lock radar to barrel direction and adjust barrel for bearing the enemy is moving to
-         *
-         *
-         *
-         * neutral/searching:
-         * - turn radar in the same direction as the gun for maximum turn rate
-         * - move in a circle around the center of the battlefield
-         *
-         * enemy spotted:
-         *
-         *  randomly stop sometimes
-         *  randomly change movement direction
-         *  prevent wall collisions
-         *
-         */
+    }
+
+    private void updateRadar(ScannedRobotEvent e) {
+        // Absolute angle towards target
+        double angleToEnemy = getHeadingRadians() + e.getBearingRadians();
+
+        // Subtract current radar heading to get the turn required to face the enemy, be sure it is normalized
+        double radarTurn = Utils.normalRelativeAngle(angleToEnemy - getRadarHeadingRadians());
+
+        // Distance we want to scan from middle of enemy to either side
+        // The 36.0 is how many units from the center of the enemy robot it scans.
+        double extraTurn = Math.min(Math.atan(SCAN_DIST / e.getDistance()), Rules.RADAR_TURN_RATE_RADIANS);
+
+        // Adjust the radar turn so it goes that much further in the direction it is going to turn
+        // Basically if we were going to turn it left, turn it even more left, if right, turn more right.
+        // This allows us to overshoot our enemy so that we get a good sweep that will not slip.
+        if (radarTurn < 0)
+            radarTurn -= extraTurn;
+        else
+            radarTurn += extraTurn;
+
+        //Turn the radar
+        setTurnRadarRightRadians(radarTurn);
+    }
+
+    private void fireAtEnemy(ScannedRobotEvent event) {
+        final double FIREPOWER = 2;
+        final double ROBOT_WIDTH = 16, ROBOT_HEIGHT = 16;
+        // Variables prefixed with e- refer to enemy, b- refer to bullet and r- refer to robot
+        final double eAbsBearing = getHeadingRadians() + event.getBearingRadians();
+        final double rX = getX(), rY = getY(),
+                bV = Rules.getBulletSpeed(FIREPOWER);
+        final double eX = rX + event.getDistance() * Math.sin(eAbsBearing),
+                eY = rY + event.getDistance() * Math.cos(eAbsBearing),
+                eV = event.getVelocity(),
+                eHd = event.getHeadingRadians();
+        // These constants make calculating the quadratic coefficients below easier
+        final double A = (eX - rX) / bV;
+        final double B = eV / bV * Math.sin(eHd);
+        final double C = (eY - rY) / bV;
+        final double D = eV / bV * Math.cos(eHd);
+        // Quadratic coefficients: a*(1/t)^2 + b*(1/t) + c = 0
+        final double a = A * A + C * C;
+        final double b = 2 * (A * B + C * D);
+        final double c = (B * B + D * D - 1);
+        final double discrim = b * b - 4 * a * c;
+        if (discrim >= 0) {
+            // Reciprocal of quadratic formula
+            final double t1 = 2 * a / (-b - Math.sqrt(discrim));
+            final double t2 = 2 * a / (-b + Math.sqrt(discrim));
+            final double t = Math.min(t1, t2) >= 0 ? Math.min(t1, t2) : Math.max(t1, t2);
+            // Assume enemy stops at walls
+            final double endX = limit(
+                    eX + eV * t * Math.sin(eHd),
+                    ROBOT_WIDTH / 2, getBattleFieldWidth() - ROBOT_WIDTH / 2);
+            final double endY = limit(
+                    eY + eV * t * Math.cos(eHd),
+                    ROBOT_HEIGHT / 2, getBattleFieldHeight() - ROBOT_HEIGHT / 2);
+            setTurnGunRightRadians(robocode.util.Utils.normalRelativeAngle(
+                    Math.atan2(endX - rX, endY - rY)
+                            - getGunHeadingRadians()));
+            setFire(FIREPOWER);
+        }
+    }
+
+    private double limit(double value, double min, double max) {
+        return Math.min(max, Math.max(min, value));
     }
 
     @Override
@@ -122,6 +179,14 @@ public class Shogouki extends AdvancedRobot {
                 && energyDiff >= 0.1 && energyDiff <= 3.0 && !wallHit;
 
 
+    }
+
+    @Override
+    public void onBulletHit(BulletHitEvent event) {
+        if (detectedEnemies.containsKey(event.getName())) {
+            var enemy = detectedEnemies.get(event.getName());
+            enemy.energy = event.getEnergy();
+        }
     }
 
     private double getBulletPower(ScannedRobotEvent e) {
@@ -217,5 +282,13 @@ public class Shogouki extends AdvancedRobot {
             angle += 2 * Math.PI;
         }
         return angle;
+    }
+
+    private void initColors() {
+        setBodyColor(new Color(118, 88, 152));
+        setGunColor(new Color(82, 208, 83));
+        setRadarColor(new Color(252, 119, 1));
+        setBulletColor(new Color(0, 0, 0));
+        setScanColor(new Color(211,41,15));
     }
 }
